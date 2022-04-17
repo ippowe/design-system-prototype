64f8ebafc0439eff61f950febd0e2042d2da92eb
Jaemin Ahn <jae7415@gmail.com>
2022-04-17 20:32:57 +0900

    feat: install react

--- packages/date/src/index.ts
+++ packages/date/src/index.ts
@@ -1,5 +1,1 @@
-// date/src/index.ts
-import { format as dateFnsFormat } from "date-fns";
-
-const DEFAULT_FORMAT = "yyyy-MM-dd HH:mm";
-export const format = (date = new Date(), formatString?: string) => dateFnsFormat(date, formatString ?? DEFAULT_FORMAT);
+export * from "./App";
